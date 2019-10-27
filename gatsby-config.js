67e08785c89beaceb658d5cbc9448de76d80402c
Ender Bonnet <enbonnet@gmail.com>
2019-10-27 12:15:02 -0300

    agregar metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Salexa - Peluquería Venezolana`,
+    description: `Peluquería Venezolana en Santiago de Chile, con estilistas de experiencia internacional`,
+    author: `@enBonnet`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

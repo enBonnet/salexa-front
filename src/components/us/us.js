018b16469697d783ecc28341fec885cdf76dc678
Ender Bonnet <enbonnet@gmail.com>
2019-10-31 21:55:23 -0300

    us component

---
+++ src/components/us/us.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+import "./us.scss"
+
+export const Us = () => {
+  return (
+    <div className="team">
+      <h2 className="section-title">Equipo</h2>
+      <div className="member">
+        <div className="title">Estilista</div>
+        <div className="image">
+          <img
+            src="https://via.placeholder.com/250"
+            alt="fotografia del equipo"
+          />
+        </div>
+        <div className="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolore
+          ducimus quo inventore ea minima maiores tenetur quasi labore quod. Et
+          magnam totam neque iure vel hic minus! Voluptatem, omnis?
+        </div>
+      </div>
+      <div className="member">
+        <div className="title">Manicurista</div>
+        <div className="image">
+          <img
+            src="https://via.placeholder.com/250"
+            alt="fotografia del equipo"
+          />
+        </div>
+        <div className="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolore
+          ducimus quo inventore ea minima maiores tenetur quasi labore quod. Et
+          magnam totam neque iure vel hic minus! Voluptatem, omnis?
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Lobby from "../components/lobby"
 import Services from "../components/services"
+import Us from "../components/us"
 
 const IndexPage = () => (
   <Layout>
@@ -17,8 +18,11 @@ const IndexPage = () => (
       message="En Santiago de Chile"
       img="https://images.pexels.com/photos/2017747/pexels-photo-2017747.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     />
-    {/* Nosotros */}
-    {/* Excelencia */}
+    <Us />
+    <Lobby
+      message="Excelencia"
+      img="https://images.pexels.com/photos/457701/pexels-photo-457701.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    />
     {/* Contacto */}
   </Layout>
 )

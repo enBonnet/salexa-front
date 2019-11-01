e43f1ee3e1fe8cc9f1fcb913702f2945e7f432a7
Ender Bonnet <enbonnet@gmail.com>
2019-10-31 21:25:03 -0300

    add services component

---
+++ src/components/services/services.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+import "./services.scss"
+
+export const Services = () => {
+  return (
+    <div className="services">
+      <h2 className="section-title">Servicios</h2>
+      <div className="service">
+        <div className="title">Peluqueria</div>
+        <div className="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolore
+          ducimus quo inventore ea minima maiores tenetur quasi labore quod. Et
+          magnam totam neque iure vel hic minus! Voluptatem, omnis?
+        </div>
+      </div>
+      <div className="service">
+        <div className="title">Peluqueria</div>
+        <div className="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolore
+          ducimus quo inventore ea minima maiores tenetur quasi labore quod. Et
+          magnam totam neque iure vel hic minus! Voluptatem, omnis?
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Lobby from "../components/lobby"
+import Services from "../components/services"
 
 const IndexPage = () => (
   <Layout>
@@ -11,25 +12,7 @@ const IndexPage = () => (
       message="Estetica Venezolana"
       img="https://images.pexels.com/photos/957882/pexels-photo-957882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
     />
-    <div className="services">
-      <h2 className="section-title">Servicios</h2>
-      <div className="service">
-        <div className="title">Peluqueria</div>
-        <div className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolore
-          ducimus quo inventore ea minima maiores tenetur quasi labore quod. Et
-          magnam totam neque iure vel hic minus! Voluptatem, omnis?
-        </div>
-      </div>
-      <div className="service">
-        <div className="title">Peluqueria</div>
-        <div className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolore
-          ducimus quo inventore ea minima maiores tenetur quasi labore quod. Et
-          magnam totam neque iure vel hic minus! Voluptatem, omnis?
-        </div>
-      </div>
-    </div>
+    <Services />
     <Lobby
       message="En Santiago de Chile"
       img="https://images.pexels.com/photos/2017747/pexels-photo-2017747.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
